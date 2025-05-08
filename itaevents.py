5aa8ef1f3229a7f8bab48c600c908fbcaf926cbb
savergiggio <44522558+savergiggio@users.noreply.github.com>
2025-05-08 17:12:29 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,7 +13,7 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 # Constants
-LINKMFP = "https://mam.lokiale.duckdns.org/"
+LINKMFP = "https://mam.lokiale.duckdns.org"
 PASSWORD = "Digimon96.."
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
