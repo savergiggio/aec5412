049ce7746c3c24fb69eee04a25fdef012ee7a1d8
savergiggio <44522558+savergiggio@users.noreply.github.com>
2025-05-08 16:53:52 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 import time
 # Costanti
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://mam.lokiale.duckdns.org/"
+PASSWORD = "Digimon96.."
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
